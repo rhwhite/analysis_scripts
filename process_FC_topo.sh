7a703c3465313822ad9d2864ca0e422797318701
Rachel White <rachelwh@cheyenne1.ib0.cheyenne.ucar.edu>
2018-07-09 12:04:22 -0600

    include exclusion of extra years

--- process_FC_topo.sh
+++ process_FC_topo.sh
@@ -9,44 +9,63 @@
 # get top level, only Atlantic, specific variables and concatenate
 dir="/glade/scratch/rachelwh/archive/"
 exp="CAM4POP_NoMThosing"
-printf -v yrstart "%04d" 1
-printf -v yrend "%04d" 200
-echo $yrstart
 
+yrstart=1
+yrend=200
+
+climstart=101
+climend=200
+
+version="cam"
+
+printf -v styrstart "%04d" $yrstart
+printf -v styrend "%04d" $yrend
+
+printf -v stclimstart "%04d" $climstart
+printf -v stclimend "%04d" $climend
+
+echo $styrstart
+
+# Create directory structure and move files
 cd ${dir}/${exp}/ocn/hist/
 mkdir raw
 
 mv ${exp}.pop.h.*.nc raw/
 cd raw
-mkdir Extra
-mv Extra/* .
+mkdir toavg
 
-mv ${exp}.pop.h.????-??.nc Extra
+# move only required files into toavg
 for iyear in `seq -f "%04g" $startyear $endyear`; do
     # record average across all monthly files for that year    
-    mv Extra/${exp}.pop.h.????-??.nc .
+    mv ${exp}.pop.h.????-??.nc toavg
 done
 
-ncrcat -O -d z_t,0,0 -d transport_reg,1,1 -v MOC,HMXL,RHO,SALT,TEMP,BSF,N_HEAT,N_SALT,TAUX,TAUY,TAREA raw/${exp}.pop.h.0* cat_limited_${exp}.pop.h.${yrstart}-${yrend}.nc
+ncrcat -O -d z_t,0,0 -d transport_reg,1,1 -v MOC,HMXL,RHO,SALT,TEMP,BSF,N_HEAT,N_SALT,TAUX,TAUY,TAREA toavg/${exp}.pop.h.0* ../cat_limited_${exp}.pop.h.${styrstart}-${styrend}.nc
 
+# move everything back out of toavg directory
+mv toavg/* .
+
+# move out of raw directory
+cd ../
 # Average over transport_ref dimension to get rid of it
-ncwa -O -a transport_reg cat_limited_${exp}.pop.h.${yrstart}-${yrend}.nc cat_limited_${exp}.pop.h.${yrstart}-${yrend}.nc
+ncwa -O -a transport_reg cat_limited_${exp}.pop.h.${styrstart}-${styrend}.nc cat_limited_${exp}.pop.h.${styrstart}-${styrend}.nc
 
-# Shifttime to get correct months (otherwise cdo reads end of Jan as 1-Feb)
-cdo yearmean -shifttime,-1mo cat_limited_${exp}.pop.h.${yrstart}-${yrend}.nc annmean_limited_${exp}.pop.h.${yrstart}-${yrend}.nc
+# Calculate annual mean with Shifttime to get correct months (otherwise cdo reads end of Jan as 1-Feb)
+cdo yearmean -shifttime,-1mo cat_limited_${exp}.pop.h.${styrstart}-${styrend}.nc annmean_limited_${exp}.pop.h.${styrstart}-${styrend}.nc
 
+# Make monthly averages
 # Need to shifttime BEFORE selecting month, which means putting it AFTER in the command
-cdo yearmean -selmon,1,2,12 -shifttime,-1mo cat_limited_${exp}.pop.h.${yrstart}-${yrend}.nc annmean_limited_${exp}.pop.h.${yrstart}-${yrend}.nc 
-cdo yearmean -selmon,3,4,5 -shifttime,-1mo cat_limited_${exp}.pop.h.${yrstart}-${yrend}.nc annmean_limited_${exp}.pop.h.${yrstart}-${yrend}.nc
-cdo yearmean -selmon,6,7,8 -shifttime,-1mo cat_limited_${exp}.pop.h.${yrstart}-${yrend}.nc annmean_limited_${exp}.pop.h.${yrstart}-${yrend}.nc
-cdo yearmean -selmon,9,10,11 -shifttime,-1mo cat_limited_${exp}.pop.h.${yrstart}-${yrend}.nc annmean_limited_${exp}.pop.h.${yrstart}-${yrend}.nc
+cdo yearmean -selmon,1,2,12 -shifttime,-1mo cat_limited_${exp}.pop.h.${styrstart}-${styrend}.nc DJFmean_limited_${exp}.pop.h.${styrstart}-${styrend}.nc 
+cdo yearmean -selmon,3,4,5 -shifttime,-1mo cat_limited_${exp}.pop.h.${styrstart}-${styrend}.nc MAMmean_limited_${exp}.pop.h.${styrstart}-${styrend}.nc
+cdo yearmean -selmon,6,7,8 -shifttime,-1mo cat_limited_${exp}.pop.h.${styrstart}-${styrend}.nc JJAmean_limited_${exp}.pop.h.${styrstart}-${styrend}.nc
+cdo yearmean -selmon,9,10,11 -shifttime,-1mo cat_limited_${exp}.pop.h.${styrstart}-${styrend}.nc SONmean_limited_${exp}.pop.h.${styrstart}-${styrend}.nc
 
 # Calculate climatologies
-ncra -d time,100,200 annmean_limited_CAM4POP_f19g16C_noMT.pop.h.${yrstart}-${yrend}.nc ANNclim_CAM4POP_f19g16C_noMT.pop.h.0001-0300.nc
-ncra -d time,100,200 DJF_limited_CAM4POP_f19g16C_noMT.pop.h.${yrstart}-${yrend}.nc DJFclim_CAM4POP_f19g16C_noMT.pop.h.0001-0300.nc
-ncra -d time,100,200 MAM_limited_CAM4POP_f19g16C_noMT.pop.h.${yrstart}-${yrend}.nc MAMclim_CAM4POP_f19g16C_noMT.pop.h.0001-0300.nc
-ncra -d time,100,200 JJA_limited_CAM4POP_f19g16C_noMT.pop.h.${yrstart}-${yrend}.nc JJAclim_CAM4POP_f19g16C_noMT.pop.h.0001-0300.nc
-ncra -d time,100,200 SON_limited_CAM4POP_f19g16C_noMT.pop.h.${yrstart}-${yrend}.nc SONclim_CAM4POP_f19g16C_noMT.pop.h.0001-0300.nc
+ncra -d time,${stclimstart},${stclimend} annmean_limited_CAM4POP_f19g16C_noMT.pop.h.${styrstart}-${styrend}.nc ANNclim_CAM4POP_f19g16C_noMT.pop.h.${stclimstart}-${stclimend}.nc
+ncra -d time,${stclimstart},${stclimend} DJF_limited_CAM4POP_f19g16C_noMT.pop.h.${styrstart}-${styrend}.nc DJFclim_CAM4POP_f19g16C_noMT.pop.h.${stclimstart}-${stclimend}.nc
+ncra -d time,${stclimstart},${stclimend} MAM_limited_CAM4POP_f19g16C_noMT.pop.h.${styrstart}-${styrend}.nc MAMclim_CAM4POP_f19g16C_noMT.pop.h.${stclimstart}-${stclimend}.nc
+ncra -d time,${stclimstart},${stclimend} JJA_limited_CAM4POP_f19g16C_noMT.pop.h.${styrstart}-${styrend}.nc JJAclim_CAM4POP_f19g16C_noMT.pop.h.${stclimstart}-${stclimend}.nc
+ncra -d time,${stclimstart},${stclimend} SON_limited_CAM4POP_f19g16C_noMT.pop.h.${styrstart}-${styrend}.nc SONclim_CAM4POP_f19g16C_noMT.pop.h.${stclimstart}-${stclimend}.nc
 
 
 # Regrid
@@ -60,10 +79,29 @@ export NCL_file='clim_CAM4POP_f19g16C_noMT.pop.h.0001-0300.nc'
 
 
 # Now do atmospheric variables
-#cd ${dir}/${exp}/atm/hist/
+cd ${dir}/${exp}/atm/hist/
 
-#ncrcat -v PRECT,TS,TREFHT,TMQ,TAUX,TAUY,SHFLX,LHFLX,RHREFHT,QFLX,PSL,PS,PRECC,PRECL,PBLH,ICEFRAC,FSUTOA,FSNTOAC,FSNTOA,FSNTC,FSNT,FSNSC,FSNS,FSDSC,FSDS,FLUTC,FLUT,FLNTC,FLNT,FLNSC,FLNS,FLDSC,FLDS,CLDTOT,CLDMED,CLDLOW,CLDHGH raw/${exp}.cam2.h0.*.nc cat_${exp}.cam2.h0.${yrstart}-${yrend}.nc
+# Create directory structure and move files
+mkdir raw
 
-#cdo yearmean -shifttime,-1mo cat_${exp}.cam2.h0.${yrstart}-${yrend}.nc annmean_${exp}.cam2.h0.${yrstart}-${yrend}.nc
+mv ${exp}.${version}.h*.nc raw/
+cd raw
+mkdir toavg
+
+# move only required files into toavg
+for iyear in `seq -f "%04g" $startyear $endyear`; do
+    # record average across all monthly files for that year    
+    mv ${exp}.${version}.h0.????-??.nc toavg
+done
+
+ncrcat -v PRECT,TS,TREFHT,TMQ,TAUX,TAUY,SHFLX,LHFLX,RHREFHT,QFLX,PSL,PS,PRECC,PRECL,PBLH,ICEFRAC,FSUTOA,FSNTOAC,FSNTOA,FSNTC,FSNT,FSNSC,FSNS,FSDSC,FSDS,FLUTC,FLUT,FLNTC,FLNT,FLNSC,FLNS,FLDSC,FLDS,CLDTOT,CLDMED,CLDLOW,CLDHGH toavg/${exp}.cam*.h0.*.nc ../cat_${exp}.${version}.h0.${styrstart}-${styrend}.nc
+
+# move everything back out of toavg directory
+mv toavg/* .
+
+# move out of raw directory
+cd ../
+
+cdo yearmean -shifttime,-1mo cat_${exp}.${version}.h0.${styrstart}-${styrend}.nc annmean_${exp}.${version}.h0.${styrstart}-${styrend}.nc
 
 
